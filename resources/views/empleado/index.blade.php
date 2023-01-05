f09c0d263e9ca1e7c73cbd62a5b4c69bd34facc5
aparra19 <angelparra67@gmail.com>
2023-01-05 17:09:54 -0400

    consulta de informacion de empleados en la BD

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,3 +1,31 @@
-<div class="contenedor">
-    <h1 class="titulo">Hola empleado!</h1>
+<div class="container">
+    <h1 class="titulo">Lista de empleados</h1>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Primer Apellido</th>
+                    <th scope="col">Segundo Apellido</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($empleados as $empleado)
+                <tr class="">
+                    <td scope="row">{{ $empleado->id}}</td>
+                    <td>{{ $empleado->Foto}}</td>
+                    <td>{{ $empleado->Nombre}}</td>
+                    <td>{{ $empleado->PrimerApellido}}</td>
+                    <td>{{ $empleado->SegundoApellido}}</td>
+                    <td>{{ $empleado->Correo}}</td>
+                    <td>Editar | Eliminar</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div> 
 </div>
