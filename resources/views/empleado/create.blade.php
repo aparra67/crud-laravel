6208c559795efc403de21e2e1c4fb448da22f9c8
aparra19 <angelparra67@gmail.com>
2023-01-06 10:42:53 -0400

    Anexo titulo identificador de formulario

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <h1 class="titulo">Aca se debe crear un empleado</h1>
+  <h1 class="titulo">Crear Empleado</h1>
   <form action="{{url('/empleado')}}" method="post" enctype="multipart/form-data">
     @csrf
     @include('empleado.form')
--- resources/views/empleado/editar.blade.php
+++ resources/views/empleado/editar.blade.php
@@ -1,3 +1,4 @@
+<h1 class="titulo">Editar Información De Empleado</h1>
 <form action="{{ url('empleado/'.$empleado->id) }}" method="post" enctype="multipart/form-data">
     @csrf
     {{ method_field('PATCH') }}
